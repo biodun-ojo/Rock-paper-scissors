227571a720ac97639a321ceb46cae5554d5f8457
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-06-27 21:41:50 +0100

    finish 5 round implementation

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
 //to convert sring to number use "parseInt"
 //how to select a particular element
-dood = [10, 11, 12]
-console.log(dood[1])
+//dood = [10, 11, 12]
+//console.log(dood[1])
 
-console.log('hello world')
+//console.log('hello world')
 
-let x = Math.random()
+//let x = Math.random()
 
-console.log(x)
+//console.log(x)
 
 
 //THE GAME CODE
-let answer = prompt("Enter an answer: rock, paper or scissors: ");
-const answers = ['rock', 'paper', 'scissors']; //computer's options
 
-// Function to get a random answer
-function computerAnswer() {
-    // Generate a random index between 0 and 2
-    const randomIndex = Math.floor(Math.random() * answers.length);
-    // Return the answer at the random index
-    return answers[randomIndex];
-}
+//scoreboard
+let humanscore = 0
+let computerscore = 0
 
-const computerchoice = computerAnswer();
-console.log(`computer anwer is ${computerchoice}`);
+//gotta make a loop for 5 rounds and also needs to add a score board
+function playgame () {
+    for (let i = 0; i < 5; i++) {
+        let answer = prompt("Enter an answer: rock, paper or scissors: ");//input
+        const answers = ['rock', 'paper', 'scissors'];
 
-function HumanAnswer() {
-    if (answer.toLowerCase() === 'rock') {
-        return 'rock'
-    } else if (answer.toLowerCase() === 'paper') {
-        return 'paper'
-    } else if (answer.toLowerCase() === 'scissors') {
-        return 'scissors'
-    }    
-}
+        function computerAnswer() {
+            // Generate a random index between 0 and 2
+            const randomIndex = Math.floor(Math.random() * answers.length);
+            // Return the answer at the random index
+            return answers[randomIndex];
+        }
 
-const humanchoice = HumanAnswer()
-console.log(`human answer is ${humanchoice}`)
+        const computerchoice = computerAnswer();
+        console.log(`computer anwer is ${computerchoice}`);
 
-function playround() {
-    if (computerchoice === humanchoice) {
-        console.log('It is a tie')
-    } else if ((humanchoice === 'rock' && computerchoice === 'scissors') ||
-    (humanchoice === 'paper' && computerchoice === 'rock') ||
-    (humanchoice === 'scissors' && computerchoice === 'paper')) {
-        console.log(`you win! ${humanchoice} beats ${computerchoice}`)
-    } else {
-        console.log(`you lose! ${computerchoice} beats ${humanchoice}`)
+        function HumanAnswer() {
+            if (answer.toLowerCase() === 'rock') {
+                return 'rock'
+            } else if (answer.toLowerCase() === 'paper') {
+                return 'paper'
+            } else if (answer.toLowerCase() === 'scissors') {
+                return 'scissors'
+            }    
+        }
+
+        const humanchoice = HumanAnswer()
+        console.log(`human answer is ${humanchoice}`)
+
+        function playround() {
+            if (computerchoice === humanchoice) {
+                console.log('It is a tie')
+            } else if ((humanchoice === 'rock' && computerchoice === 'scissors') ||
+            (humanchoice === 'paper' && computerchoice === 'rock') ||
+            (humanchoice === 'scissors' && computerchoice === 'paper')) {
+                humanscore += 1
+                console.log(`you win! ${humanchoice} beats ${computerchoice}`)
+                console.log(`human score is ${humanscore} and  computer score is ${computerscore}`)
+            } else {
+                computerscore += 1
+                console.log(`you lose! ${computerchoice} beats ${humanchoice}`)
+                console.log(`human score is ${humanscore} and  computer score is ${computerscore}`)
+            }
+        }
+        
+        playround()
     }
 }
 
-playround()
-
-//gotta make a loop for 5 rounds and also needs to add a score board
+playgame()
