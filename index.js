861e01b413f9e1fc79beeec262e4ba35331044dd
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-07-11 20:43:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,33 +8,9 @@ const scissors = document.getElementById('scissors')
 let humanscore = 0
 let computerscore = 0
 
-//gotta make a loop for 5 rounds and also needs to add a score board
+
 //function playgame () {
-    /*   let answer = prompt("Enter an answer: rock, paper or scissors: ");     //input by alert     */
     const answers = ['rock', 'paper', 'scissors'];
-
-    /* function computerAnswer() {
-        // Generate a random index between 0 and 2
-        const randomIndex = Math.floor(Math.random() * answers.length);
-        // Return the answer at the random index
-        return answers[randomIndex];
-    } */
-
-    //const computerchoice = computerAnswer();
-    //console.log(`computer anwer is ${computerchoice}`);
-
-    /* function HumanAnswer() {
-        if (answer.toLowerCase() === 'rock') {
-            return 'rock'
-        } else if (answer.toLowerCase() === 'paper') {
-            return 'paper'
-        } else if (answer.toLowerCase() === 'scissors') {
-            return 'scissors'
-        }    
-    }
-
-    const humanchoice = HumanAnswer()
-    console.log(`human answer is ${humanchoice}`) */
 
     function playround(humanchoice) {
 
@@ -65,13 +41,10 @@ let computerscore = 0
             console.log(`human score is ${humanscore} and  computer score is ${computerscore}`)
         }
     }
-
     /* for (let i = 0; i < 5; i++) {
         
     } */
 //}
-
-//playgame()
 
 rock.addEventListener('click', () => playround('rock'))
 paper.addEventListener('click', () => playround('paper'))
